8b6a573dd1d05a51ad6bd66107e1b6ed0f16f84a
TjayAmit <tristanjayamit0813@gmail.com>
2025-08-10 12:01:14 +0800

    feat: complete login process for admin and tenant

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,10 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
-use Spatie\Permission\Models\Role;
-use Spatie\Permission\Models\Permission;
 
 class User extends Authenticatable
 {
